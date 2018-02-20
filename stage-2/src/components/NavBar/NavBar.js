4fd328882fc22f5c3dc9d1ff6bedb76706a8c3d5
Zac Cericola <zcericola@gmail.com>
2018-02-20 14:28:26 -0600

    finished stage 2

--- stage-2/src/components/NavBar/NavBar.js
+++ stage-2/src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './NavBar.css';
+import { Link } from 'react-router-dom';
 import Logo from '../../images/dev-mountain-logo.png'
 
 class NavBar extends Component {
